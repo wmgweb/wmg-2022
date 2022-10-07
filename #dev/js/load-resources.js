2c7b34d3d17cbe62d8da7c0e5d0f5c757ce18895
Josh <josh@grid-digital.co.uk>
2022-10-07 13:39:29 +0100

    Fix to load-resources.js

--- #dev/js/load-resources.js
+++ #dev/js/load-resources.js
@@ -1,44 +1,56 @@
-var version = '2';
+jQuery(function ($) {
 
-// Get current script directory to load local files
-var scripts = document.getElementsByTagName('script');
-var currentScript = scripts[scripts.length-1];
-var currentScriptSrc = currentScript.src;
-var currentDirectory = currentScriptSrc.substring(0, currentScriptSrc.lastIndexOf('/')) + '/';
+	var version = '2';
 
-// Load all url params
-var urlParams = new URLSearchParams(window.location.search);
+	// Get all scripts on the page
+	var scripts = document.getElementsByTagName('script');
 
-// // Check if cache param is set to 0
-if(urlParams.get('cache') == 0) {
-	// If so set version to timestamp
-    version = Date.now();
-}
+	// Loop through and find this script
+	var currentScript = '';
+	$.each(scripts, function(key, value) {
+		if(value.src.indexOf('load-resources.js') >= 0) {
+			currentScript = value.src;
+		}
+	});
 
-// Load Additional CSS - can add additional files to this array or push to it within if statements to load page-specific scripts
-var css_files = [
-	currentDirectory + 'main.css'
-];
+	// Get current script directory
+	var currentDirectory = currentScript.substring(0, currentScript.lastIndexOf('/')) + '/';
 
-// Loop through array, create link tag and append to end of head
-css_files.forEach(function (item, index) {
-  	var link = document.createElement('link');
-  	link.href = item + '?v=' + version;
-  	link.rel = 'stylesheet';
-	link.type = 'text/css';
-	document.getElementsByTagName('head')[0].appendChild(link);
-});
+	// Load all url params
+	var urlParams = new URLSearchParams(window.location.search);
 
-// Load Additional JS - can add additional files to this array or push to it within if statements to load page-specific scripts
-var js_files = [
-	currentDirectory + 'main.js'
-];
+	// // Check if cache param is set to 0
+	if(urlParams.get('cache') == 0) {
+		// If so set version to timestamp
+		version = Date.now();
+	}
 
-// Loop through array, create script tag and append to end of head
-js_files.forEach(function (item, index) {
-  	var script = document.createElement('script');
-  	script.src = item + '?v=' + version;
-	script.type = 'text/javascript';
-	script.async = 'true';
-	document.getElementsByTagName('head')[0].appendChild(script);
+	// Load Additional CSS - can add additional files to this array or push to it within if statements to load page-specific scripts
+	var css_files = [
+		currentDirectory + 'main.css'
+	];
+
+	// Loop through array, create link tag and append to end of head
+	css_files.forEach(function (item, index) {
+		var link = document.createElement('link');
+		link.href = item + '?v=' + version;
+		link.rel = 'stylesheet';
+		link.type = 'text/css';
+		document.getElementsByTagName('head')[0].appendChild(link);
+	});
+
+	// Load Additional JS - can add additional files to this array or push to it within if statements to load page-specific scripts
+	var js_files = [
+		currentDirectory + 'main.js'
+	];
+
+	// Loop through array, create script tag and append to end of head
+	js_files.forEach(function (item, index) {
+		var script = document.createElement('script');
+		script.src = item + '?v=' + version;
+		script.type = 'text/javascript';
+		script.async = 'true';
+		document.getElementsByTagName('head')[0].appendChild(script);
+	});
+
 });

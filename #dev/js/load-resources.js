5d7c969618b735e07b43795889d000bc6ca3688b
Josh <josh@grid-digital.co.uk>
2022-10-07 13:06:19 +0100

    Change to load-resources to load local files.

--- #dev/js/load-resources.js
+++ #dev/js/load-resources.js
@@ -1,9 +1,15 @@
-var version = '1';
+var version = '2';
+
+// Get current script directory to load local files
+var scripts = document.getElementsByTagName('script');
+var currentScript = scripts[scripts.length-1];
+var currentScriptSrc = currentScript.src;
+var currentDirectory = currentScriptSrc.substring(0, currentScriptSrc.lastIndexOf('/')) + '/';
 
 // Load all url params
 var urlParams = new URLSearchParams(window.location.search);
 
-// Check if cache param is set to 0
+// // Check if cache param is set to 0
 if(urlParams.get('cache') == 0) {
 	// If so set version to timestamp
     version = Date.now();
@@ -11,12 +17,8 @@ if(urlParams.get('cache') == 0) {
 
 // Load Additional CSS - can add additional files to this array or push to it within if statements to load page-specific scripts
 var css_files = [
-	'https://wmgwebcdn.wmg.warwick.ac.uk/wmg/production/dist/main.css'
+	currentDirectory + 'main.css'
 ];
-
-// if(urlslug = 'people') {
-// 	css_files.push('https://wmgwebcdn.wmg.warwick.ac.uk/wmg/production/dist/main.css');
-// }
 
 // Loop through array, create link tag and append to end of head
 css_files.forEach(function (item, index) {
@@ -29,7 +31,7 @@ css_files.forEach(function (item, index) {
 
 // Load Additional JS - can add additional files to this array or push to it within if statements to load page-specific scripts
 var js_files = [
-	'https://wmgwebcdn.wmg.warwick.ac.uk/wmg/production/dist/main.js'
+	currentDirectory + 'main.js'
 ];
 
 // Loop through array, create script tag and append to end of head
